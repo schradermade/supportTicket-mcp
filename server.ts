95e1d191ed46885b96d8758352cd595476395bbc
Nathan Schrader <natecschrader@gmail.com>
2025-04-01 14:33:52 -0700

    initial mcp logic and client creation

--- server.ts
+++ server.ts
@@ -1,8 +1,41 @@
-// server.ts
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Request, Response } from 'express';
 
+// anthropic sdk
+import { Anthropic } from '@anthropic-ai/sdk';
+import { MessageParam, Tool } from '@anthropic-ai/sdk/resources/index.mjs';
+
+// mcp sdk
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+
+const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
+if (!ANTHROPIC_API_KEY) {
+  throw new Error('ANTHROPIC_API_KEY is not set');
+}
+
+class MCPCLient {
+  private mcp: Client;
+  private llm: Anthropic;
+  private transport: StdioClientTransport | null = null;
+  private tools: Tool[] = [];
+
+  constructor() {
+    this.llm = new Anthropic({
+      apiKey: ANTHROPIC_API_KEY,
+    });
+    this.mcp = new Client({ name: 'mcp-client-cli', version: '1.0.0' });
+  }
+
+  // connect to mcp server
+
+  // process query
+}
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Simple route to check if the server is working
 app.get('/', (req: Request, res: Response) => {
